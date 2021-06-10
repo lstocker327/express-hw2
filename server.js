7eb935c40fbcc1746ea36fd46e987b87ecdaee58
lstocker327 <lukestocker1@gmail.com>
2021-06-09 22:07:56 -0400

    magic 8 ball express application created

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = 3000
+const PORT = 3000;
+
+let magic8quotes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
 app.get("/greeting/:name", function(req,res){
   res.send("hello " + req.params.name);
@@ -11,6 +13,9 @@ app.get("/tip/:total/:tipPercentage", function(req,res){
   res.send("your tip is " + tip)
 })
 
+app.get("/magic/:question", function(req,res){
+  res.send(req.params.question + "<br>" + "<h1>" + magic8quotes[Math.floor(Math.random() * 19)] + "</h1>");
+})
 
 app.listen(PORT, function(){
   console.log('listening on port' + PORT);
