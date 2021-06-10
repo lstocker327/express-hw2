10c41df31fb4942a83e7fefb7342aafb0d81c18b
lstocker327 <lukestocker1@gmail.com>
2021-06-09 21:40:41 -0400

    Tip calculator express application creted

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/greeting/:name", function(req,res){
   res.send("hello " + req.params.name);
 });
 
+app.get("/tip/:total/:tipPercentage", function(req,res){
+  let tip = parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100);
+  res.send("your tip is " + tip)
+})
 
 
 app.listen(PORT, function(){
